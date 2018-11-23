d35e8fa9a81ac08f89c922868827a02555821794
Robert McCary <robert.mccary@yahoo.com>
2018-11-23 17:57:38 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,4 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot("Squawk!")# Create method `parrot` that outputs a given phrase and
+puts "Squawk"# returns the phrase
+return "Squawk"
+end
